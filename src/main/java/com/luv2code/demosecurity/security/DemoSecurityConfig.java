d4dc7b6b32e449534916c98e84316c14c71f5a74
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-06 15:41:00 -0300

    Restrict URLs Based on Roles - PT3

--- src/main/java/com/luv2code/demosecurity/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/demosecurity/security/DemoSecurityConfig.java
@@ -36,7 +36,11 @@ public class DemoSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer -> configurer.anyRequest().authenticated())
+        http.authorizeHttpRequests(configurer -> configurer
+                        .requestMatchers("/").hasRole("EMPLOYEE")
+                        .requestMatchers("/leaders/**").hasRole("MANAGER")
+                        .requestMatchers("/systems/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin(form ->
                         form.loginPage("/showMyLoginPage")
                         .loginProcessingUrl("/authenticateTheUser")
@@ -45,16 +49,4 @@ public class DemoSecurityConfig {
 
         return http.build();
     }
-
-
-
-
-
-
-
-
-
-
-
-
 }
